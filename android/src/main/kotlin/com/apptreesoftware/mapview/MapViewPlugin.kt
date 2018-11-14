daa303863a8a74febc202530c04d7f36ac2c8e79
manbach <35201025+manbach@users.noreply.github.com>
2018-11-14 11:22:21 +0100

    Update MapViewPlugin.kt
    Bugfix

--- android/src/main/kotlin/com/apptreesoftware/mapview/MapViewPlugin.kt
+++ android/src/main/kotlin/com/apptreesoftware/mapview/MapViewPlugin.kt
@@ -205,17 +205,17 @@ class MapViewPlugin(val activity: Activity) : MethodCallHandler {
                     return
                 }
                 val mapOptions = call.argument<Map<String, Any>>("mapOptions")
-                val cameraDict = mapOptions["cameraPosition"] as Map<String, Any>
+                val cameraDict = mapOptions?["cameraPosition"] as Map<String, Any>
                 initialCameraPosition = getCameraPosition(cameraDict)
                 toolbarActions = getToolbarActions(call.argument<List<Map<String, Any>>>("actions"))
-                showUserLocation = mapOptions["showUserLocation"] as Boolean
-                showMyLocationButton = mapOptions["showMyLocationButton"] as Boolean
-                showCompassButton = mapOptions["showCompassButton"] as Boolean
-                hideToolbar = mapOptions["hideToolbar"] as Boolean
-                mapTitle = mapOptions["title"] as String
+                showUserLocation = mapOptions?["showUserLocation"] as Boolean
+                showMyLocationButton = mapOptions?["showMyLocationButton"] as Boolean
+                showCompassButton = mapOptions?["showCompassButton"] as Boolean
+                hideToolbar = mapOptions?["hideToolbar"] as Boolean
+                mapTitle = mapOptions?["title"] as String
 
                 if (mapOptions["mapViewType"] != null) {
-                    val mappedMapType: Int? = mapTypeMapping.get(mapOptions["mapViewType"]);
+                    val mappedMapType = mapTypeMapping?.get(mapOptions["mapViewType"]);
                     if (mappedMapType != null) mapViewType = mappedMapType;
                 }
 
